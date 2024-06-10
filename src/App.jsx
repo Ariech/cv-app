8bf04aa4c45893c18676c05e15ae2f0a58fe45e6
Dominik <dominikant99@gmail.com>
2024-06-10 13:06:38 +0200

    Add form handling for EducationInfo component
    Add handleArrayInputChange function to handle state updates for inputs within an array
    Update handleSingleInputChange function to use new naming convention
    Implement EducationInfo component with form inputs for school, degree, fromDate, and toDate.

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,27 @@ import GeneralInfo from "./components/GeneralInfo";
 import WorkExperience from "./components/WorkExperience";
 
 function App() {
+  const generateId = () => {
+    return new Date().getTime().toString();
+  };
+
   const [generalInfo, setGeneralInfo] = useState({
     name: "",
     email: "",
     phone: "",
   });
 
-  const handleChange = (e, setState) => {
+  const [educationInfo, setEducationInfo] = useState([
+    {
+      id: generateId(),
+      school: "",
+      degree: "",
+      fromDate: "",
+      toDate: "",
+    },
+  ]);
+
+  const handleSingleInputChange = (e, setState) => {
     const { value, name } = e.target;
 
     setState((prevInfo) => ({
@@ -19,13 +33,37 @@ function App() {
     }));
   };
 
+  const handleArrayInputChange = (e, setState, id) => {
+    const { value, name } = e.target;
+
+    setState((prevInfo) =>
+      prevInfo.map((info) => {
+        if (info.id === id) {
+          return { ...info, [name]: value };
+        }
+
+        return info;
+      })
+    );
+  };
+
   return (
     <>
       <GeneralInfo
         state={generalInfo}
-        onInputChange={(e) => handleChange(e, setGeneralInfo)}
+        onInputChange={(e) => handleSingleInputChange(e, setGeneralInfo)}
       />
-      <EducationInfo />
+
+      {educationInfo.map((info) => (
+        <EducationInfo
+          key={info.id}
+          state={info}
+          onInputChange={(e) =>
+            handleArrayInputChange(e, setEducationInfo, info.id)
+          }
+        />
+      ))}
+
       <WorkExperience />
     </>
   );
--- src/components/EducationInfo.jsx
+++ src/components/EducationInfo.jsx
@@ -1,7 +1,49 @@
-function EducationInfo() {
+function EducationInfo({ state, onInputChange }) {
+  const { school, degree, fromDate, toDate } = state;
+
   return (
     <>
-      <p>Education Info</p>
+      <form>
+        <label>
+          School name:
+          <input
+            type="text"
+            name="school"
+            value={school}
+            onChange={onInputChange}
+          />
+        </label>
+
+        <label>
+          Degree:
+          <input
+            type="text"
+            name="degree"
+            value={degree}
+            onChange={onInputChange}
+          />
+        </label>
+
+        <label>
+          From Date:
+          <input
+            type="date"
+            value={fromDate}
+            name="fromDate"
+            onChange={onInputChange}
+          />
+        </label>
+
+        <label>
+          To Date:
+          <input
+            type="date"
+            value={toDate}
+            name="toDate"
+            onChange={onInputChange}
+          />
+        </label>
+      </form>
     </>
   );
 }
